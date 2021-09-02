f323518c285d121b7cdeb76631407ef8ffe96c7f
Konstantinas Jariomenko <k.jariomenko@gmail.com>
2021-09-02 17:51:50 +0300

    01

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is our app!!!</h1>
+      <p>The sky is blue and the grass is green.</p>
+    </div>
+  );
+}
+
+export default App;
